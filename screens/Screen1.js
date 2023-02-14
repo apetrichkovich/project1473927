d4b0395886b7087cf5edef5fceb131ca10cb0926
apetrichkovich <alexander.petrichkovich@gmail.com>
2023-02-14 16:10:33 +0200

    3 pages.

--- screens/Screen1.js
+++ screens/Screen1.js
@@ -1,14 +1,36 @@
-import { Button } from 'react-native';
+import { Button, StyleSheet, ImageBackground } from 'react-native';
+
+const image = {uri: 'https://lifeimg.pravda.com/images/doc/e/2/e259fa6-skola.jpg'};
 
 const Screen1 = ({navigation}) => {
     return (
-        <Button
-            title="Экран 2"
-            onPress={() =>
-                navigation.navigate('Экран 2', {name: 'Jane'})
-            }
-        />
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+            <Button
+                title="OK"
+                onPress={() =>
+                    navigation.navigate('Экран 2', {name: 'Jane'})
+                }
+            />
+        </ImageBackground>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+        fontSize: 42,
+        lineHeight: 84,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        backgroundColor: '#000000c0',
+    },
+});
 
 export default Screen1;
