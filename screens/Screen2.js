d9fb74f0ed61f441b9709f2393def59b2c5e5a9d
apetrichkovich <alexander.petrichkovich@gmail.com>
2023-02-15 00:15:58 +0200

    Screen 2.

--- screens/Screen2.js
+++ screens/Screen2.js
@@ -1,14 +1,69 @@
-import { Button } from 'react-native';
+import {Button, StyleSheet, View, Image} from 'react-native';
+import {Component} from 'react';
 
-const Screen2 = ({navigation}) => {
-    return (
-        <Button
-            title="Экран 3"
-            onPress={() =>
-                navigation.navigate('Экран 3', {name: 'Jane'})
+const image1 = {uri: 'https://lifeimg.pravda.com/images/doc/e/2/e259fa6-skola.jpg'};
+const image1i = {uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKr5wT7rfkjkGvNeqgXjBmarC5ZNoZs-H2uMpML8O7Q4F9W-IlUQibBT6IPqyvX45NOgw&usqp=CAU'};
+
+export default class Screen2 extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            current: 0,
+            angels: [180, 180, 180, 180, 180],
+            images: [image1, image1, image1, image1, image1]
+        }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.rotate(),
+            1
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    rotate() {
+        const state = this.state;
+        state.angels[state.current] -= 2;
+        if (state.angels[state.current] === 90) {
+            state.images[state.current] = image1i;
+        }
+        if (state.angels[state.current] === 0) {
+            if (state.current < 4) {
+                state.current++;
+            } else {
+                clearInterval(this.timerID);
             }
-        />
-    );
+        }
+        this.setState(state);
+    }
+
+    render() {
+        return (
+            <View>
+                <View style={styles.view}>
+                    <Image source={this.state.images[0]} style={[styles.image, {transform: [{rotateX: this.state.angels[0] + 'deg'}]}]}/>
+                    <Image source={this.state.images[1]} style={[styles.image, {transform: [{rotateX: this.state.angels[1] + 'deg'}]}]}/>
+                    <Image source={this.state.images[2]} style={[styles.image, {transform: [{rotateX: this.state.angels[2] + 'deg'}]}]}/>
+                    <Image source={this.state.images[3]} style={[styles.image, {transform: [{rotateX: this.state.angels[3] + 'deg'}]}]}/>
+                    <Image source={this.state.images[4]} style={[styles.image, {transform: [{rotateX: this.state.angels[4] + 'deg'}]}]}/>
+                </View>
+                <Button title="Экран 3" onPress={() => this.props.navigation.navigate('Экран 3', {name: 'Jane'})}/>
+            </View>
+        );
+    }
 };
 
-export default Screen2;
+const styles = StyleSheet.create({
+    view: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    image: {
+        width: 100,
+        height: 100,
+    },
+});
